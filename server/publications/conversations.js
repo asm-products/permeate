5459c7f0b48c783fcc5c6313a374d527375e221a
Josh Matz <josh.matz@gmail.com>
2015-01-29 07:33:20 -0600

    finally semi-working routing and conversation pages

--- server/publications/conversations.js
+++ server/publications/conversations.js
@@ -1,10 +1,13 @@
-Meteor.publish('conversationsList', function(owner, options) {
+Meteor.publish('conversationList', function(owner, options) {
   check(owner, String);
 
   check(options, {
     sort: Object,
     limit: Number
   });
+
+  console.log('owner: ', owner);
+  console.log('options: ', options);
 
   return Conversations.find({owner: owner}, options);
 });
